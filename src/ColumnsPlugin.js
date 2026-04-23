6ebc891d4479619e87c2425e7a074fb374a80868
Den Arustamov <g000mail000is000very000foolish@gmail.com>
2026-04-23 15:29:28 +0300

    main: upd

--- src/ColumnsPlugin.js
+++ src/ColumnsPlugin.js
@@ -1,5 +1,5 @@
-import Plugin from "@ckeditor/ckeditor5-core";
-import Collection from "@ckeditor/ckeditor5-utils";
+import { Plugin } from "@ckeditor/ckeditor5-core";
+import { Collection } from "@ckeditor/ckeditor5-utils";
 import { addListToDropdown, createDropdown } from "@ckeditor/ckeditor5-ui";
 
 import "./columns.css";
